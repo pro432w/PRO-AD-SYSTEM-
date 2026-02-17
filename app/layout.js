c178786e20370d306107014dce17227c5fa1a84d
pro432w <protonproh@protonmail.com>
2026-02-17 11:18:34 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-import Script from 'next/script'
-
 export const metadata = {
   title: 'PRO ADs SYSTEM',
 }
@@ -9,15 +7,23 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script src="https://telegram.org/js/telegram-web-app.js"></script>
+        {/* Force Load Monetag Script */}
+        <script 
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d, z, s) {
+                s.src = '//libtl.com/sdk.js';
+                s.async = true;
+                s.dataset.zone = z;
+                s.dataset.sdk = 'show_10619740';
+                d.head.appendChild(s);
+              })(document, '10619740', document.createElement('script'));
+            `
+          }} 
+        />
       </head>
       <body style={{ margin: 0, padding: 0, backgroundColor: '#f0f9ff', color: '#1e3a8a', fontFamily: 'sans-serif' }}>
         {children}
-        <Script 
-          src="//libtl.com/sdk.js" 
-          strategy="lazyOnload" 
-          data-zone="10619740" 
-          data-sdk="show_10619740"
-        />
       </body>
     </html>
   )
